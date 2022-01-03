322919d6e7a8f2359d4aea1f0e167c4bb6a717ce
Stephen Montague <me@montaguestephen.com>
2022-01-02 22:25:04 -0500

    Updates to App file to add the Header component and outline what we are going to do with the rest of the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Lets build the facebook clone!</h1>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<Header />
+
+			{/* App body */}
+			{/* Sidebar */}
+			{/* Feed */}
+			{/* Widgets */}
+		</div>
+	);
 }
 
 export default App;
