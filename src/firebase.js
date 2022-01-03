eb1a3dc61845eeb7f67c077ccb8482a15fd5706a
Stephen Montague <me@montaguestephen.com>
2022-01-02 22:24:20 -0500

    Addition of a firebase config file which will be our database for this project.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQz298qP0BncJ_qlGwKY-8wcupmfwEZx4",
+  authDomain: "facebook-clone-a2463.firebaseapp.com",
+  projectId: "facebook-clone-a2463",
+  storageBucket: "facebook-clone-a2463.appspot.com",
+  messagingSenderId: "612415382740",
+  appId: "1:612415382740:web:8414bf6952715beefc8741",
+  measurementId: "G-3CFEVB5TYC"
+};
